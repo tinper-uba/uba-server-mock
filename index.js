6eda8b59aea02c308b12e098628c8df24b4af52b
kvkens <kvkens@qq.com>
2018-01-03 09:37:12 +0800

    remove:去掉bodyParser

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /**
  * mock server plugin for uba-server
- * Date : 2018-01-02 14:33:12
+ * Date : 2018-01-03 09:34:59
  */
 
 const Router = require("koa-router");
-const bodyParser = require("koa-bodyparser");
+// const bodyParser = require("koa-bodyparser");
 const utils = require("./utils");
 
 module.exports = (app, opt) => {
   let router = new Router();
   utils.loadMock(router,opt.file);
-  app.use(bodyParser());
+  // app.use(bodyParser());
   app.use(router.routes());
   app.use(router.allowedMethods());
 }
